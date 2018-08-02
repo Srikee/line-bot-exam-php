15cae6e14c26e88f60f08894259ce9685cdcc027
Srikee <kee19384@gmail.com>
2018-08-02 18:04:43 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,10 +1,7 @@
 <?php // callback.php
-
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
-
 $access_token = 'R27qpC+nfEewcJovnr0ORUia5hIRgZHRWEkjEqY9cARAWEk20r4I6s+3OkiTKzmN+AUXTBp+GsueSi+krTQbXrIXte/Cv0vV/phIAZrrtCiXhJcZTKd93R6iq9Xrllt/XZtBQibFA7x10F2Eh9TLHgdB04t89/1O/w1cDnyilFU=';
-
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -16,6 +13,10 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			//$userId = $event['source']['userId'];
+			
+			$map = map_data($event['message']['text']);
+			send($event, $map[0], $map[1]);
+/*
 			switch($event['message']['text'])	{
 				case "เปิดอุปกรณ์ 1" : send($event, "เปิดอุปกรณ์ 1 แล้วค่ะ", "http://kscyber.ddns.net/?D=1&I=1"); break;
 				case "ปิดอุปกรณ์ 1" : send($event, "ปิดอุปกรณ์ 1 แล้วค่ะ", "http://kscyber.ddns.net/?D=1&I=0"); break;
@@ -26,11 +27,63 @@ if (!is_null($events['events'])) {
 				case "เปิดทั้งหมด" : send($event, "เปิดทั้งหมด แล้วค่ะ", "http://kscyber.ddns.net/open_all"); break;
 				case "ปิดทั้งหมด" : send($event, "ปิดทั้งหมด แล้วค่ะ", "http://kscyber.ddns.net/close_all"); break;
 				default : send($event, "ไม่พบคำสั่ง\n\nพิมพ์คำสั่งดังนี้นะค่ะ\nเปิดอุปกรณ์ 1\nเปิดอุปกรณ์ 2\nเปิดอุปกรณ์ 3\nปิดอุปกรณ์ 1\nปิดอุปกรณ์ 2\nปิดอุปกรณ์ 3\nเปิดทั้งหมด\nปิดทั้งหมด"); break;
-			}
+			}*/
 		}
 	}
 }
 echo "OK";
+function map_data($text) {
+	$arr = array(
+		array(
+			"text"=>"เปิดอุปกรณ์ 1",
+			"similar"=>array("เปิด 1", "เปิดหนึ่ง", "เปิดไฟ 1", "เปิดไฟหนึ่ง"),
+			"url"=>"http://kscyber.ddns.net/?D=1&I=1"
+		),
+		array(
+			"text"=>"เปิดอุปกรณ์ 2",
+			"similar"=>array("เปิด 2", "เปิดสอง", "เปิดไฟ 2", "เปิดไฟสอง"),
+			"url"=>"http://kscyber.ddns.net/?D=2&I=1"
+		),
+		array(
+			"text"=>"เปิดอุปกรณ์ 3",
+			"similar"=>array("เปิด 3", "เปิดสาม", "เปิดไฟ 3", "เปิดไฟสาม"),
+			"url"=>"http://kscyber.ddns.net/?D=3&I=1"
+		),
+		array(
+			"text"=>"ปิดอุปกรณ์ 1",
+			"similar"=>array("ปิด 1", "ปิดหนึ่ง", "ปิดไฟ 1", "ปิดไฟหนึ่ง"),
+			"url"=>"http://kscyber.ddns.net/?D=1&I=0"
+		),
+		array(
+			"text"=>"ปิดอุปกรณ์ 2",
+			"similar"=>array("ปิด 2", "ปิดสอง", "ปิดไฟ 2", "ปิดไฟสอง"),
+			"url"=>"http://kscyber.ddns.net/?D=2&I=0"
+		),
+		array(
+			"text"=>"ปิดอุปกรณ์ 3",
+			"similar"=>array("ปิด 3", "ปิดสาม", "ปิดไฟ 3", "ปิดไฟสาม"),
+			"url"=>"http://kscyber.ddns.net/?D=3&I=0"
+		),
+		array(
+			"text"=>"เปิดทั้งหมด",
+			"similar"=>array("เปิดทุกอย่าง", "เปิดทุกตัว", "เปิดหมด"),
+			"url"=>"http://kscyber.ddns.net/open_all"
+		),
+		array(
+			"text"=>"ปิดทั้งหมด",
+			"similar"=>array("ปิดทุกอย่าง", "ปิดทุกตัว", "ปิดหมด"),
+			"url"=>"http://kscyber.ddns.net/close_all"
+		),
+	);
+	foreach ($arr as $key => $value) {
+		$t = $value["text"];
+		if( $t==$text ) return array($t." แล้วค่ะ", $value["url"]);
+		foreach ($value["similar"] as $key2 => $value2) {
+			if( $value2==$text ) return array($t." แล้วค่ะ", $value["url"]);
+		}
+	}
+	return array($t."ไม่พบคำสั่ง\n\nพิมพ์คำสั่งดังนี้นะค่ะ\nเปิดอุปกรณ์ 1\nเปิดอุปกรณ์ 2\nเปิดอุปกรณ์ 3\nปิดอุปกรณ์ 1\nปิดอุปกรณ์ 2\nปิดอุปกรณ์ 3\nเปิดทั้งหมด\nปิดทั้งหมด", "");
+}
 function get_content($arr_data) {
 	$url = $arr_data["url"];
 	$postdata = http_build_query($arr_data);
